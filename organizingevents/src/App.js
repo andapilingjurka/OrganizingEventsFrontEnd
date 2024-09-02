3e4b4b1e692d29188a22d3b44a1afdb632a83838
andapilingjurka <ap56170@ubt-uni.net>
2024-09-02 11:36:49 -0700

    Rregullimi i path per 'EventCategories', 'EventThemes' dhe 'Events' ne App.js

--- organizingevents/src/App.js
+++ organizingevents/src/App.js
@@ -3,6 +3,9 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap-icons/font/bootstrap-icons.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Dashboard from "./Admin/Dashboard";
+import EventThemesAdmin from "./Admin/EventThemesAdmin";
+import EventCategoriesAdmin from "./Admin/EventCategoriesAdmin";
+import EventsAdmin from "./Admin/EventsAdmin";
 
 
 
@@ -11,7 +14,9 @@ function App() {
     <Router>
       <Routes>
         <Route path="/dashboard" element={<Dashboard />} />
- 
+        <Route path="/eventsAdmin" element={<EventsAdmin />} />
+        <Route path="/eventThemesAdmin" element={<EventThemesAdmin />} />
+        <Route path="/eventCategoriesAdmin" element={<EventCategoriesAdmin />} />
       </Routes>
     </Router>
   );
