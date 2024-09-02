987bd697da30c7e89492b80302d23e673c230ca8
andapilingjurka <ap56170@ubt-uni.net>
2024-09-02 11:31:44 -0700

    Rregullimi i path per 'Dashboard' ne App.js

--- organizingevents/src/App.js
+++ organizingevents/src/App.js
@@ -1,9 +1,19 @@
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap-icons/font/bootstrap-icons.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Dashboard from "./Admin/Dashboard";
+
+
 
 function App() {
   return (
-    <div>
-
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/dashboard" element={<Dashboard />} />
+ 
+      </Routes>
+    </Router>
   );
 }
 
