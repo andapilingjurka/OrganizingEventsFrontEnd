061bbd36a8c976abcde4a64e98a95e7180e74881
Shaqir Topko <topkoshaqir@gmail.com>
2024-09-04 14:16:38 +0200

    Rregullimi i ikonave

--- organizingevents/src/Admin/include/Sidebar.js
+++ organizingevents/src/Admin/include/Sidebar.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link, useLocation } from "react-router-dom";
-import { FaUserShield, FaHome, FaUsers, FaCalendarDay, FaTag, FaListAlt, FaSignOutAlt } from "react-icons/fa";
+import { FaUserShield, FaHome, FaUsers, FaCalendarDay, FaTag, FaListAlt, FaSignOutAlt, FaConciergeBell, FaUtensils } from "react-icons/fa";
 import "../style.css";
 
 function Sidebar() {
