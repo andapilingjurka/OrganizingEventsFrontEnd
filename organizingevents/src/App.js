e23269d741b2c1b0bab4f766a58aa01875848eeb
FestimZylfiu <fz57195@ubt-uni.net>
2024-09-04 20:52:28 +0200

    Rregullimi i path per “Staff” ne App.js

--- organizingevents/src/App.js
+++ organizingevents/src/App.js
@@ -6,8 +6,8 @@ import Dashboard from "./Admin/Dashboard";
 import EventThemesAdmin from "./Admin/EventThemesAdmin";
 import EventCategoriesAdmin from "./Admin/EventCategoriesAdmin";
 import EventsAdmin from "./Admin/EventsAdmin";
-import RestaurantTypesAdmin from "./Admin/RestaurantTypesAdmin";
-import RestaurantsAdmin from "./Admin/RestaurantsAdmin";
+import StaffAdmin from "./Admin/StaffAdmin"; 
+
 
 
 function App() {
@@ -18,8 +18,7 @@ function App() {
         <Route path="/eventsAdmin" element={<EventsAdmin />} />
         <Route path="/eventThemesAdmin" element={<EventThemesAdmin />} />
         <Route path="/eventCategoriesAdmin" element={<EventCategoriesAdmin />} />
-        <Route path="/restaurantTypesAdmin" element={<RestaurantTypesAdmin />} />
-        <Route path="/restaurantsAdmin" element={<RestaurantsAdmin />} />
+        <Route path="/staffAdmin" element={<StaffAdmin />} />
       </Routes>
     </Router>
   );
