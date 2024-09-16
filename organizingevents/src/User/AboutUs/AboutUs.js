28ac27114320a2510ab060473791349009df9fa3
Shaqir Topko <topkoshaqir@gmail.com>
2024-09-16 16:37:36 +0200

    Krijimi i fajllave per page AboutUs

---
+++ organizingevents/src/User/AboutUs/AboutUs.js
@@ -0,0 +1,20 @@
+// AboutUs.js
+import React from 'react';
+import Navbar from '../include/Navbar';
+import Footer from '../include/Footer';
+import './AboutUs.css';
+
+
+const AboutUs = () => {
+  return (
+    <div>
+      <Navbar />
+      
+     
+
+      { <Footer /> }
+    </div>
+  );
+};
+
+export default AboutUs;
