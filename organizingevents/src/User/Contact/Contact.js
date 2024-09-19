c028f12caeaa0d824f30183bbedb8675338b43f6
FestimZylfiu <fz57195@ubt-uni.net>
2024-09-20 00:37:21 +0200

    Shtimi i funksionalitetit EmailJS

--- organizingevents/src/User/Contact/Contact.js
+++ organizingevents/src/User/Contact/Contact.js
@@ -3,9 +3,10 @@ import axios from "axios";
 import { motion } from "framer-motion";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./contact.css";
+import emailjs from '@emailjs/browser';
 
 import Navbar from '../include/Navbar';
-
+import flower from '../images/flower.png'; 
 
 const variants = {
     initial: {
@@ -31,6 +32,10 @@ function Contact() {
     const [isAlertVisible, setIsAlertVisible] = useState(false);
     const [isSubmitting, setIsSubmitting] = useState(false);
 
+    const formRef = useRef();
+    const [error, setError] = useState(false);
+    const [success, setSuccess] = useState(false);
+
     useEffect(() => {
         Load();
     }, []);
@@ -45,8 +50,6 @@ function Contact() {
     }
 
     async function save(event) {
-        event.preventDefault();
-        setIsSubmitting(true);
         try {
             await axios.post('https://localhost:7214/api/Contact/Add', {
                 name: name,
@@ -74,6 +77,37 @@ function Contact() {
         }, 4000);
     }
 
+    const sendEmail = (e) => {
+        return emailjs
+            .sendForm('service_qi4am4r', 'template_5vvrue1', formRef.current, 'dazq5w4_RQabMTCVL')
+            .then(
+                () => {
+                    setSuccess(true);
+                    setError(false);
+                },
+                (error) => {
+                    console.log(error);
+                    setSuccess(false);
+                    setError(true);
+                }
+            );
+    };
+
+    const handleFormSubmit = async (event) => {
+        event.preventDefault();
+        setIsSubmitting(true);
+        setSuccess(false);
+        setError(false);
+
+        try {
+            await sendEmail(); 
+            await save(); 
+        } catch (err) {
+            setError(true);
+            showAlert("Failed to send the message.", "alert-danger-contact");
+        }
+    };
+
     return (
         <div>
             <Navbar />
@@ -86,64 +120,79 @@ function Contact() {
                 viewport={{ once: true }}
             >
                 <div className="formContainer contact-form-container">
-                    <motion.h2 variants={variants}>Contact Us</motion.h2>
-                    <motion.form onSubmit={save}>
-                        <div className="form-group-contact">
-                            <label htmlFor="name">Full Name</label>
-                            <input
-                                type="text"
-                                id="name"
-                                className="form-control"
-                                placeholder="Full Name"
-                                value={name}
-                                onChange={(e) => setName(e.target.value)}
-                                required
-                            />
-                        </div>
-                        <div className="form-group-contact">
-                            <label htmlFor="email">Email</label>
-                            <input
-                                type="email"
-                                id="email"
-                                className="form-control"
-                                placeholder="Email"
-                                value={email}
-                                onChange={(e) => setEmail(e.target.value)}
-                                required
-                            />
-                        </div>
-                        <div className="form-group-contact">
-                            <label htmlFor="message">Message</label>
-                            <textarea
-                                id="message"
-                                className="form-control"
-                                placeholder="Your Message"
-                                value={message}
-                                onChange={(e) => setMessage(e.target.value)}
-                                required
-                            />
-                        </div>
-                        <div className="text-center">
-                            <button
-                                type="submit"
-                                className="btn-contact"
-                                disabled={isSubmitting}
-                                style={{ backgroundColor: "#ff7b54" }}
-                            >
-                                {isSubmitting ? "Sending..." : "Send"}
-                            </button>
-                        </div>
-                    </motion.form>
+                    <div className="contact-wrapper">
+                        <motion.img 
+                            src={flower} 
+                            alt="Flower" 
+                            className="contact-image" 
+                            variants={variants} 
+                        />
+                        <motion.form
+                            ref={formRef}
+                            onSubmit={handleFormSubmit}
+                            className="contact-form"
+                        >
+                            <motion.h2 variants={variants}>Contact Us</motion.h2>
+                            <div className="form-group-contact">
+                                <label htmlFor="name">Full Name</label>
+                                <input
+                                    type="text"
+                                    id="name"
+                                    name="name"
+                                    className="form-control"
+                                    placeholder="Full Name"
+                                    value={name}
+                                    onChange={(e) => setName(e.target.value)}
+                                    required
+                                />
+                            </div>
+                            <div className="form-group-contact">
+                                <label htmlFor="email">Email</label>
+                                <input
+                                    type="email"
+                                    id="email"
+                                    name="email"
+                                    className="form-control"
+                                    placeholder="Email"
+                                    value={email}
+                                    onChange={(e) => setEmail(e.target.value)}
+                                    required
+                                />
+                            </div>
+                            <div className="form-group-contact">
+                                <label htmlFor="message">Message</label>
+                                <textarea
+                                    id="message"
+                                    name="message"
+                                    className="form-control"
+                                    placeholder="Your Message"
+                                    value={message}
+                                    onChange={(e) => setMessage(e.target.value)}
+                                    required
+                                />
+                            </div>
+                            <div className="text-center">
+                                <button
+                                    type="submit"
+                                    className="btn-contact"
+                                    disabled={isSubmitting}
+                                    style={{ backgroundColor: "#ff7b54" }}
+                                >
+                                    {isSubmitting ? "Sending..." : "Send"}
+                                </button>
+                                {error && <p className="text-danger">Error sending the message</p>}
+                                {success && <p className="text-success">Message sent successfully!</p>}
+                            </div>
 
-                    {isAlertVisible && (
-                        <div className={`alert-contact ${alertType}`}>
-                            {alertMessage}
-                        </div>
-                    )}
+                            {isAlertVisible && (
+                                <div className={`alert-contact ${alertType}`}>
+                                    {alertMessage}
+                                </div>
+                            )}
+                        </motion.form>
+                    </div>
                 </div>
             </motion.div>
-
-           
         </div>
     );
 }
